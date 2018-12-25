e8d758957344b7151c3134c525c1bfb1ab8b3256
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2018-12-25 15:35:56 -0300

    Add view for a layout and admin. Add the routes for resource 'service'.
    The admin view is the dashboard to create, edit and delete the services
    store in the database. It will list the services in a table.

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,32 @@
+@extends('layout')
+
+@section('content')
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+  <h1 class="h2">Services</h1>
+  <div class="btn-toolbar mb-2 mb-md-0">
+    <form method="GET" action="/services/create">
+      <input type="submit" class="btn btn-sm btn-outline-secondary" value="Create Service">
+    </form> 
+  </div>
+</div>
+<div class="table-responsive">
+  <table class="table table-striped table-sm">
+    <thead>
+      <tr>
+        <th>{{ $columns[0] }}</th>
+        <th>{{ $columns[1] }}</th>
+        <th>{{ $columns[4] }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($services as $service)
+      <tr class="table-items-row">
+        <td><a href="/services/{{ $service->id }}">{{ $service->id }}</a></td>
+        <td><a href="/services/{{ $service->id }}">{{ $service->title }}</a></td>
+        <td><a href="/services/{{ $service->id }}">{{ $service->city }}</a></td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+</div> 
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/admin', 'ServicesController@index');
+
+Route::resource('services', 'ServicesController');
