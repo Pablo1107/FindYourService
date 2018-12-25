b4384d4016442085990989c55db0444078d55a78
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2018-12-25 16:24:50 -0300

    Add edit and delete buttons to show view

--- resources/views/services/show.blade.php
+++ resources/views/services/show.blade.php
@@ -1,12 +1,19 @@
 @extends('layout')
 
 @section('content')
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
   <h1 class="h2">{{ $service->title }}</h1>
   <div class="btn-toolbar mb-2 mb-md-0">
-    <form method="GET" action="/services/{{ $service->id }}/edit">
-      <input type="submit" class="btn btn-sm btn-outline-secondary" value="Edit">
-    </form> 
+    <div class="btn-group mr-2">
+      <form method="GET" action="/services/{{ $service->id }}/edit">
+        <input type="submit" class="btn btn-sm btn-outline-secondary" value="Edit">
+      </form> 
+      <form method="POST" action="/services/{{ $service->id }}">
+        @method('DELETE')
+        @csrf
+        <input type="submit" class="btn btn-danger btn-sm btn-outline-secondary" value="Delete">
+      </form> 
+    </div> 
   </div>
 </div>
 <p>{{ $service->description }}</p> 
