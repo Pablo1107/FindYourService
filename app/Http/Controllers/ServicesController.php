803b0f983fe507b06a161de1424c8b0c8cbf555e
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2019-01-02 18:03:35 -0300

    Change ServicesController to deny guest access to all the API excluding public and list

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -8,6 +8,10 @@ use App\Services\Google;
 
 class ServicesController extends Controller
 {
+  public function __construct()
+  {
+    $this->middleware('auth')->except(['public', 'list']);
+  }
   /**
    * Display a listing of the resource.
    *
